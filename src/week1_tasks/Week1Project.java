8f03690f05d86fcc19e49328d74c2956286a694c
Karim_Cydeo <smoot10@hotmail.com>
2023-05-22 22:03:25 -0400

    1 code of java

--- src/week1_tasks/Week1Project.java
+++ src/week1_tasks/Week1Project.java
@@ -3,4 +3,16 @@ package week1_tasks;
 public class Week1Project {
 
     // Please start project for week 1
-}
+
+    public static void main(String[] args) {
+
+        int num1 = 5;
+
+        int num2 = 6;
+
+    }
+
+
+    }
+
+
