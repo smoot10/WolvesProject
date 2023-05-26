7899ec9121a0816b096725adbbe200c07bf377b2
Karim_Cydeo <smoot10@hotmail.com>
2023-05-25 21:02:11 -0400

    task1 OddEven

--- src/week1_tasks/Week1Project.java
+++ src/week1_tasks/Week1Project.java
@@ -2,17 +2,41 @@ package week1_tasks;
 
 public class Week1Project {
 
-    // Please start project for week 1
 
-    public static void main(String[] args) {
 
-        int num1 = 5;
+        public static void main(String[] args) {
+            int num1 = 6;
+            int num2 = 5;
 
-        int num2 = 6;
+            boolean isEven= 1 % 2 == 0;
+            System.out.println(num1 + " is an even number");//output :Even
+
+            boolean isOdd = num2 % 2  !=0;
+            System.out.println(num2 + " is an odd number");//output :Odd
+
+
+
+        }
+
+
 
     }
 
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
