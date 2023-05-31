7017ee98b4cc6ff7cc8c4a2077dc2679a764af25
Karim_Cydeo <smoot10@hotmail.com>
2023-05-30 23:11:30 -0400

    Divisible numbers update

---
+++ src/week1_tasks/OddEven.java
@@ -0,0 +1,43 @@
+package week1_tasks;
+
+public class OddEven {
+
+
+
+        public static void main(String[] args) {
+            int num1 = 6;
+            int num2 = 5;
+
+            boolean isEven= 1 % 2 == 0;
+            System.out.println(num1 + " is an even number");//output :Even
+
+            boolean isOdd = num2 % 2  !=0;
+            System.out.println(num2 + " is an odd number");//output :Odd
+
+
+
+        }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/week2_tasks/Week2Project.java
+++ src/week2_tasks/Week2Project.java
@@ -4,4 +4,6 @@ public class Week2Project {
 
     // Please start week 2 project
 
+    public static class SwapNumbers {
+    }
 }
